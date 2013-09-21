5791e29937f285b97e8173ac961346add4879420
pe.reimann@gmail.com <pe.reimann@gmail.com@266907ef-c027-45b0-d0fd-4d2863d3e254>
2013-09-21 15:37:39 +0000

    etapas do fluxo

---
+++ SGOA/src/java/converter/TipoServicoConverter.java
@@ -0,0 +1,61 @@
+package converter;
+
+import facede.ImagemEtapaFacade;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import model.ImagemEtapa;
+import org.hibernate.Session;
+import util.HibernateFactory;
+
+@FacesConverter(value = "imagemEtapaConverter", forClass = ImagemEtapa.class)
+public class TipoServicoConverter implements Converter {
+
+    public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+        if (value == null || value.length() == 0) {
+            return null;
+        }
+        try {
+            Integer.parseInt(value);
+        } catch (Exception e) {
+            return null;
+        }
+        try {
+            Session sessao = HibernateFactory.currentSession();
+            ImagemEtapaFacade ebjImagemEtapa = new ImagemEtapaFacade();
+            return ebjImagemEtapa.obterPorId(sessao, getKey(value));
+        } catch (Exception ex) {
+            Logger.getLogger(TipoServicoConverter.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        } finally {
+            HibernateFactory.closeSession();
+        }
+    }
+
+    java.lang.Integer getKey(String value) {
+        java.lang.Integer key;
+        key = Integer.valueOf(value);
+        return key;
+    }
+
+    String getStringKey(java.lang.Integer value) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(value);
+        return sb.toString();
+    }
+
+    public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+        if (object == null || object == "") {
+            return null;
+        }
+        if (object instanceof ImagemEtapa) {
+            ImagemEtapa o = (ImagemEtapa) object;
+            return getStringKey(o.getId());
+        } else {
+            throw new IllegalArgumentException("object " + object + " is of type " + object.getClass().getName()); 
+        }
+    }
+}
--- SGOA/src/java/model/ImagemEtapa.java
+++ SGOA/src/java/model/ImagemEtapa.java
@@ -1,11 +1,27 @@
 package model;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import model.Base.BaseEntidadeDescricao;
 
 @Entity
 @Table(name = "imagem_etapa")
 public class ImagemEtapa extends BaseEntidadeDescricao<ImagemEtapa> {
+    
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "titulo")
+    private String titulo;   
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }  
     
 }
