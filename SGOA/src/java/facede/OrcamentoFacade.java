15fab64665875e5cac93cb900409a07123a2be9f
pe.reimann@gmail.com <pe.reimann@gmail.com@266907ef-c027-45b0-d0fd-4d2863d3e254>
2013-09-27 02:52:17 +0000

    orcamento

--- SGOA/src/java/facede/OrcamentoFacade.java
+++ SGOA/src/java/facede/OrcamentoFacade.java
@@ -1,6 +1,8 @@
 package facede;
 
 import facede.base.BaseFacade;
+import java.util.Calendar;
+import java.util.Date;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,6 +15,7 @@ import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import util.HibernateFactory;
 
 @Stateless
 public class OrcamentoFacade extends BaseFacade<Orcamento> {
@@ -29,6 +32,56 @@ public class OrcamentoFacade extends BaseFacade<Orcamento> {
         super(Orcamento.class);
     }
 
+    @Override
+    public void incluir(Session sessao, Orcamento item) throws Exception {
+        if (sessao == null) {
+            throw new Exception("Sessão não iniciada.");
+        }
+        try {
+            HibernateFactory.beginTransaction();
+            Calendar cal = Calendar.getInstance();
+            Date date = new Date();
+            cal.setTime(date);
+            int ano = cal.get(Calendar.YEAR);
+            item.setDataCadastro(cal.getTime());
+            item.setAno(ano);
+            item.setSituacao('A'); //Em Aberto 
+            sessao.save(item);
+            item.setNumero(ObterNumero(sessao, ano));
+            HibernateFactory.currentSession().update(item);
+            HibernateFactory.commitTransaction();
+        } catch (Exception e) {
+            HibernateFactory.rollbackTransaction();
+            throw e;
+        }
+    }
+
+    public Integer ObterNumero(Session sessao, int ano) throws Exception {
+        if (sessao == null) {
+            throw new Exception("Sessão não iniciada.");
+        }
+        Criteria c = sessao.createCriteria(Orcamento.class);
+        c.add(Restrictions.eq("ano", ano));
+        Integer numero = ((Integer) c.setProjection(Projections.max("numero")).uniqueResult());
+        if (numero == null) {
+            numero = 1;
+        } else {
+            numero++;
+        }
+        return numero;
+    }
+
+    public Orcamento ObterPorAnoENumero(Session sessao, int ano, int numero) throws Exception {
+        if (sessao == null) {
+            throw new Exception("Sessão não iniciada.");
+        }
+        Criteria c = sessao.createCriteria(Orcamento.class);
+        c.add(Restrictions.eq("ano", ano));
+        c.add(Restrictions.eq("numero", numero));
+        Orcamento orcamento = ((Orcamento) c.uniqueResult());
+        return orcamento;
+    }
+
     public List<Orcamento> selecionarPorParametros(Session sessao, String sort, SortOrder order, Integer page, Integer maxPage, String nomeFiltro, String placaFiltro) throws Exception {
         if (sessao == null) {
             throw new Exception("Sessão não iniciada.");
@@ -40,10 +93,10 @@ public class OrcamentoFacade extends BaseFacade<Orcamento> {
             c.add(Restrictions.like("pessoa.nome", nomeFiltro, MatchMode.ANYWHERE).ignoreCase());
         }
         /*if (placaFiltro != null && !placaFiltro.isEmpty()) {
-            DetachedCriteria veiculosCriteria = DetachedCriteria.forClass(Veiculo.class, "veiculos");
-            veiculosCriteria.add(Restrictions.like("veiculos.placa", placaFiltro, MatchMode.EXACT).ignoreCase());
-            veiculosCriteria.add(Restrictions.eqProperty("veiculos.pessoa.id", "cli.pessoa.id"));
-            c.add(Subqueries.exists(veiculosCriteria.setProjection(Projections.property("veiculos.id"))));
+        DetachedCriteria veiculosCriteria = DetachedCriteria.forClass(Veiculo.class, "veiculos");
+        veiculosCriteria.add(Restrictions.like("veiculos.placa", placaFiltro, MatchMode.EXACT).ignoreCase());
+        veiculosCriteria.add(Restrictions.eqProperty("veiculos.pessoa.id", "cli.pessoa.id"));
+        c.add(Subqueries.exists(veiculosCriteria.setProjection(Projections.property("veiculos.id"))));
         }*/
         super.setRowCount((Long) c.setProjection(Projections.rowCount()).uniqueResult());
         c.setProjection(null).setResultTransformer(Criteria.ROOT_ENTITY);
@@ -60,14 +113,12 @@ public class OrcamentoFacade extends BaseFacade<Orcamento> {
             c.add(Restrictions.like("pessoa.nome", nomeFiltro, MatchMode.ANYWHERE).ignoreCase());
         }
         /*if (placaFiltro != null && !placaFiltro.isEmpty()) {
-            DetachedCriteria veiculosCriteria = DetachedCriteria.forClass(Veiculo.class, "veiculos");
-            veiculosCriteria.add(Restrictions.like("veiculos.placa", placaFiltro, MatchMode.EXACT).ignoreCase());
-            veiculosCriteria.add(Restrictions.eqProperty("veiculos.pessoa.id", "cli.pessoa.id"));
-            c.add(Subqueries.exists(veiculosCriteria.setProjection(Projections.property("veiculos.id"))));
+        DetachedCriteria veiculosCriteria = DetachedCriteria.forClass(Veiculo.class, "veiculos");
+        veiculosCriteria.add(Restrictions.like("veiculos.placa", placaFiltro, MatchMode.EXACT).ignoreCase());
+        veiculosCriteria.add(Restrictions.eqProperty("veiculos.pessoa.id", "cli.pessoa.id"));
+        c.add(Subqueries.exists(veiculosCriteria.setProjection(Projections.property("veiculos.id"))));
         }*/
         List<Orcamento> lista = c.list();
         return lista;
     }
-
-    
 }
---
+++ SGOA/src/java/facede/VeiculoFacade.java
@@ -0,0 +1,25 @@
+package facede;
+
+import facede.base.BaseFacade;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import model.Veiculo;
+
+@Stateless
+public class VeiculoFacade extends BaseFacade<Veiculo> {
+
+    @PersistenceContext(unitName = "SGOAPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public VeiculoFacade() {
+        super(Veiculo.class);
+    }
+
+   
+}
